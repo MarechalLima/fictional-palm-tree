01d40b05e0260b9f19782fb762e10ed3362c13d1
Nicholas L. S. Silva <nicholas.silva@laccan.ufal.br>
2021-04-11 17:32:43 -0300

    Quest 4

--- graphsS7.py
+++ graphsS7.py
@@ -6,22 +6,25 @@ import numpy as np
 
 # Menu
 def main():
-    choice = int(input("Escolha a questão a exibir {1, 2, 3, 4, 5, 6, 7}:\n"))
+    while(True):
+        choice = int(input("Escolha a questão a exibir {1, 2, 3, 4, 5, 6, 7} \n0 para sair:\n"))
 
-    if choice == 1:
-        questOne()
-    elif choice == 2:
-        questTwo()
-    elif choice == 3:
-        questThree()
-    elif choice == 4:
-        print("fodase")
-    elif choice == 5:
-        print("fodase")
-    elif choice == 6:
-        print("fodase")
-    elif choice == 7:
-        print("fodase")
+        if choice == 1:
+            questOne()
+        elif choice == 2:
+            questTwo()
+        elif choice == 3:
+            questThree()
+        elif choice == 4:
+            questFour()
+        elif choice == 5:
+            print("fodase")
+        elif choice == 6:
+            print("fodase")
+        elif choice == 7:
+            print("fodase")
+        elif choice == 0:
+            break
 
 ## Questão 1
 def questOne():
@@ -40,14 +43,21 @@ def questOne():
 def questTwo():
     data = pd.read_csv("data/feature_counts.txt", delim_whitespace=True)
 
-    data.plot.barh(x='Feature', y='Count', legend=False, color="lightgray", edgecolor='black')
+    fig, ax = plt.subplots()
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+
+    data.plot.barh(x='Feature', y='Count', legend=False, color="lightgray", edgecolor='black', ax=ax)
     plt.xlabel("Number of features")
 
     plt.xticks([0, (2*10**4), (4*10**4),(6*10**4)])
     plt.tight_layout()
-    plt.box(False)
-    plt.show()
     plt.savefig("Quest2.png")
+    plt.show()
+
+    del fig
+    del ax
+
 
 ## Questão 3
 def questThree():
@@ -68,5 +78,33 @@ def questThree():
     plt.savefig("Quest3.png")
     plt.show()
 
+    del fig
+    del ax
+
+## Questão 4
+def questFour():
+    data = pd.read_csv("data/male_female_counts.txt", delim_whitespace=True)
+    
+    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+                '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+
+    fig, ax = plt.subplots()
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+
+    data.plot.bar(x='Sample', y='Count', legend=False, color=colors, ax=ax)
+
+    plt.yticks([0,5,10,15])
+    plt.xticks(rotation=0, fontsize=8)
+
+    plt.xlabel("")
+    plt.tight_layout()
+    plt.savefig("Quest4.png")
+    plt.show()
+
+    del fig
+    del ax
+
+# questFour()
 
 main()
